6cca6e683930fa2f35d4a29d9e61dfbf615b1e97
k0nnte <bystrov.igor25@gmail.com>
2025-05-13 01:24:55 +0300

    feat: complite all task

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,22 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 5, b: 5, action: Action.Add, expected: 10 },
+  { a: 5, b: 5, action: Action.Subtract, expected: 0 },
+  { a: 5, b: 5, action: Action.Multiply, expected: 25 },
+  { a: 5, b: 5, action: Action.Divide, expected: 1 },
+  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  { a: 2, b: 2, action: null, expected: null },
+  { a: null, b: null, action: Action.Exponentiate, expected: null },
+  // continue cases for other actions
+];
 
 describe('simpleCalculator', () => {
   // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('test', ({ a, b, action, expected }) => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
   });
   // Consider to use Jest table tests API to test all cases above
 });
--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,12 @@
 // Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import path from 'path';
+import fs from 'fs';
+import fsp from 'fs/promises';
+import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+
+jest.mock('path');
+jest.mock('fs');
+jest.mock('fs/promises');
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -11,11 +18,19 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    const a = jest.fn();
+    const spy = jest.spyOn(global, 'setTimeout');
+    doStuffByTimeout(a, 100);
+    expect(spy).toHaveBeenCalledWith(a, 100);
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    const a = jest.fn();
+    doStuffByTimeout(a, 100);
+    expect(a).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(100);
+    expect(a).toHaveBeenCalledTimes(1);
   });
 });
 
@@ -29,24 +44,40 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    const a = jest.fn();
+    const spy = jest.spyOn(global, 'setInterval');
+    doStuffByInterval(a, 100);
+    expect(spy).toHaveBeenCalledWith(a, 100);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const a = jest.fn();
+    doStuffByInterval(a, 100);
+    jest.advanceTimersByTime(400);
+    expect(a).toHaveBeenCalledTimes(4);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    const fPath = 'q.txt';
+    await readFileAsynchronously(fPath);
+    expect(path.join).toHaveBeenCalledWith(__dirname, fPath);
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    (fs.existsSync as jest.Mock).mockReturnValue(false);
+
+    const rez = await readFileAsynchronously('ss.txt');
+    expect(rez).toBeNull();
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    const a = 'hi';
+    (fsp.readFile as jest.Mock).mockReturnValue(Buffer.from(a));
+    (path.join as jest.Mock).mockReturnValue('hi.txt');
+    (fs.existsSync as jest.Mock).mockReturnValue(true);
+    const rez = await readFileAsynchronously('hi.txt');
+    expect(rez).toBe(a);
   });
 });
